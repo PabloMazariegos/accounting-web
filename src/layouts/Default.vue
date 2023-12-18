bf453b721e89bbfa50d068a25aa83030fb001ee0
PabloMazariegos <pmazariegospinto@gmail.com>
2023-12-17 21:17:48 -0600

    remove unnecesary code

---
+++ src/layouts/Default.vue
@@ -0,0 +1,10 @@
+<template>
+  <v-layout class="rounded rounded-md">
+    <v-app-bar title="Accounting Web"></v-app-bar>
+    <v-main>
+      <v-container fluid>
+        <router-view></router-view>
+      </v-container>
+    </v-main>
+  </v-layout>
+</template>
