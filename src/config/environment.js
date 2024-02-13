7cb695ac8ac37fb8f9430052c787180ed93aa1ff
PabloMazariegos <pmazariegospinto@gmail.com>
2024-02-12 19:09:21 -0600

    remove code, migrate to next js

--- src/config/environment.js
+++
@@ -1,7 +0,0 @@
-export default {
-  baseUrl: import.meta.env.VITE_ACCOUNTING_API_BASE_URL || 'http://localhost:5000',
-  maxAttemps: import.meta.env.VITE_MAX_ATTEMPTS || 3,
-  retryDelay: import.meta.env.VITE_RETRY_DELAY || 1000,
-  defaultPageSize: 10,
-
-}
